c33f5f565ded58298833397a43077226494c9939
David Elkan <delkan@locusrobotics.com>
2018-09-05 11:43:19 -0400

    cleaning up mikrotik api, adding bsd, removing duplicate code

--- wireless_watcher/src/wireless_watcher/mikrotik_api.py
+++ wireless_watcher/src/wireless_watcher/mikrotik_api.py
@@ -1,6 +1,42 @@
-#!/usr/bin/python
+# Software License Agreement (BSD License)
+#
+#  Copyright (c) 2018, Locus Robotics
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#  # Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#  # Redistributions in binary form must reproduce the above
+#     copyright notice, this list of conditions and the following
+#     disclaimer in the documentation and/or other materials provided
+#     with the distribution.
+#  # Neither the name of the copyright holder nor the names of its
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
 
-import sys, posix, time, md5, binascii, socket, select
+import binascii
+import md5
+import posix
+import time
+import socket
+import select
+import sys
 
 
 class RouterOSApi(object):
@@ -11,7 +47,6 @@ class RouterOSApi(object):
         self.currenttag = 0
 
     def login(self, username="admin", pwd="", verbose=True):
-
         for repl, attrs in self.talk(["/login", "=name=" + username, "=password=" + pwd], verbose):
             if repl == '!trap':
                 raise ValueError("Failed to login, check username and password and try again.")
@@ -149,20 +184,6 @@ class RouterOSApi(object):
                 raise RuntimeError, "connection closed by remote end"
             ret += s
         return ret
-    def silentGet(self, command_list):
-        return self.talk(command_list, verbose=False)
-    
-    def getAllInterfaces(self):
-        return self.talk(["/interface/getall"], verbose=False)
-    
-    def getWirelessInterfaces(self):
-        return self.silentGet(["/interface/print", "?type=wlan"])
-    
-    def getEthernetInterfaces(self):
-        return self.silentGet(["/interface/print", "?type=ether"])
-    
-    def monitorWirelessInterface(self, interface="wlan1"):
-        return self.silentGet(["/interface/wireless/monitor","=numbers={}".format(interface),"=once="])
 
 
 def main():
